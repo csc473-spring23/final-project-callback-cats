64946e49962211890497292c0e302bd526601f35
CrystalYang108 <crystyang108@gmail.com>
2023-04-26 23:13:08 -0400

    added navbar in components folder

---
+++ components/Navbar.js
@@ -0,0 +1,43 @@
+import { NavLink } from 'react-router-dom'
+// import { ReactComponent as Brand } from '../../assets/icons/logo.svg'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <h3>Callback Cats</h3>
+        </div>
+        <div className="nav-elements">
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/catfacts">Cat Facts</NavLink>
+              </li>
+              <li>
+                <NavLink to="/buy">Buy</NavLink>
+              </li>
+              <li>
+                <NavLink to="/sell">Sell</NavLink>
+              </li>
+              <li>
+                <NavLink to="/trade">Trade</NavLink>
+              </li>
+              <li className="loginreg">
+                <NavLink to="/loginreg">Login/Register</NavLink>
+              </li>
+            </ul>
+        
+        </div>
+         
+      </div>
+      
+       
+    </nav>
+  )
+}
+
+export default Navbar
