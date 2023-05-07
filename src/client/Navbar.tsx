721b23996725b731b7951be80719df74daf1f4e6
CrystalYang108 <crystyang108@gmail.com>
2023-05-06 22:13:45 -0400

    added about to navbar

--- src/client/Navbar.tsx
+++ src/client/Navbar.tsx
@@ -12,6 +12,9 @@ const Navbar = () => {
         </div>
         <div className='nav-elements'>
           <ul>
+          <li>
+              <NavLink to='/home'>Home</NavLink>
+            </li>
             <li>
               <NavLink to='/catfacts'>Cat Facts</NavLink>
             </li>
