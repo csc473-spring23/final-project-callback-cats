903df43bcde1a16a1fecff8e5083fc19a43ea1f5
reazs <reaz.shakil2@gmail.com>
2023-05-10 13:39:06 -0400

    added catinfocard

--- src/client/User_info/Userinfo.tsx
+++ src/client/User_info/Userinfo.tsx
@@ -3,7 +3,7 @@ import "./Userinfo.css";
 import LoginNavbar from "../Components/LoginNavBar";
 import Login from "../Login/Login";
 import React, { useState, useEffect } from "react";
-
+import CatInfoCard from "../components/CatInfoCard";
 type Cats = {
   age: number;
   breed: string;
@@ -118,41 +118,9 @@ function UserInfo() {
       </div>
       <div className=" w-[90%] grid md:grid-cols-3 grid-cols-2 gap-4 mx-[5%]">
         {filterCats.map((cat) => (
-          <div key={cat.id} className="shadow-md bg-white p-4 rounded-lg">
-            <div className="delete-icon-container">
-              <i className="fa-solid fa-xmark text-gray-300 hover:text-red-500 cursor-pointer text-[30px] mb-3"></i>
-            </div>
-            <img className="rounded-sm" src={cat.img_url} alt={cat.img_url} />
-            <div className="cat-info">
-              <p>
-                <article>
-                  <strong>Name : </strong>
-                  {cat.name}
-                </article>
-                <article>
-                  <strong>Age : </strong>
-                  {cat.age}
-                </article>
-                <article>
-                  <strong>Breed : </strong>
-                  {cat.breed}
-                </article>
-                <article>
-                  <strong>Gender : </strong>
-                  {cat.gender}
-                </article>
-                <article>
-                  <strong>Is it available? : </strong>
-                  {cat.is_available ? <>Yes</> : <>No</>}
-                </article>
-                <article>
-                  <strong>Description : </strong>
-                  <>{cat.description}</>
-                </article>
-                <div className=""></div>
-              </p>
-            </div>
-          </div>
+          <>
+            <CatInfoCard cat={cat} />
+          </>
         ))}
       </div>
 
