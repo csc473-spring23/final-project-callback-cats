0de96388e10adf39bce0a74e172d92777fbb38e0
asamiul101 <58337911+samiularnob12@users.noreply.github.com>
2023-05-09 23:53:01 -0400

    changes for adopt page

--- src/client/Adopt_page/Adoptpage.tsx
+++ src/client/Adopt_page/Adoptpage.tsx
@@ -1,13 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { ReactNode, useEffect, useState } from "react";
+import "./Adoptpage.css";
+
+type Cats = {
+  age: number;
+  breed: string;
+  description: string;
+  gender: string;
+  id: number;
+  img_url: string;
+  is_available: boolean;
+  name: string;
+  seller_email: string;
+  seller_id: number;
+  seller_name: string;
+};
 
 function Adoptpage() {
+  const [cats, setCats] = useState<Cats[]>([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/all_cats', {
-      method: 'GET',
+    fetch("http://127.0.0.1:5000/all_cats", {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-      }
+        "Content-Type": "application/json",
+      },
     })
       .then((res) => {
         if (res.status === 200) {
@@ -17,20 +33,73 @@ function Adoptpage() {
       .then((data) => {
         console.log(data);
         if (data.code === 400) {
-          alert('Not cat found');
+          alert("Not cat found");
         } else {
-          console.log(data);
+          console.log(data.body);
+          const catData = data.body.map((cat: any) => ({
+            age: cat.age,
+            breed: cat.breed,
+            description: cat.description,
+            gender: cat.gender,
+            id: cat.id,
+            img_url: cat.img_url,
+            is_available: cat.is_available,
+            name: cat.name,
+            seller_email: cat.seller_email,
+            seller_id: cat.seller_id,
+            seller_name: cat.seller_name,
+          }));
+          setCats(catData);
         }
       })
       .catch((error) => {
-        console.log('error');
+        console.log("error");
       });
+  }, []);
 
-  },[])
-
-  return <div>
-
-  </div>;
+  return (
+    <>
+      <div className="adoptTitle">
+        <h1>Choose Your Favourite Cat</h1>
+      </div>
+      <div className="adoptBody">
+        {cats.map((cat) => (
+          <div key={cat.id} className="each-cat">
+            <img src={cat.img_url} alt={cat.img_url} />
+            <div className="cat-info">
+              <p>
+                <p>Donating by {cat.seller_name.toUpperCase()}</p>
+                <article>
+                  <strong>Name : </strong>
+                  {cat.name}
+                </article>
+                <article>
+                  <strong>Age : </strong>
+                  {cat.age}
+                </article>
+                <article>
+                  <strong>Breed : </strong>
+                  {cat.breed}
+                </article>
+                <article>
+                  <strong>Gender : </strong>
+                  {cat.gender}
+                </article>
+                <article>
+                  <strong>Is it available? : </strong>
+                  {cat.is_available ? <>Yes</> : <>No</>}
+                </article>
+                <article>
+                  <strong>Description : </strong>
+                  <>{cat.description}</>
+                </article>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Adoptpage;
